37d83eebc4fc866d02217154ddc386baa7791a2e
Piotr <lewczykpiotr@gmail.com>
2022-12-16 17:41:00 +0100

    add Gallery component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
   <v-app :theme="theme">
-
     <SideBar @toggle-theme="toggleTheme" :current-theme="theme" />
-
     <v-main>
-
+      <Gallery />
     </v-main>
-
   </v-app>
 </template>
 
 <script>
 import SideBar from './components/SideBar.vue'
+import Gallery from './components/Gallery.vue'
 
 export default {
   name: 'App',
   components: {
-    SideBar
+    SideBar,
+    Gallery
   },
   data() {
     return {
-      theme: 'light'
+      theme: 'dark'
     }
   },
   methods: {
---
+++ src/components/Gallery.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-row>
+        <v-col v-for="n in 240" :key="n" cols="1">
+            <v-card>
+                <v-img :src="`https://picsum.photos/500/300?image=${n * 4}`"
+                    :lazy-src="`https://picsum.photos/10/6?image=${n * 4}`" aspect-ratio="1" cover>
+                    <template v-slot:placeholder>
+                        <v-row class="fill-height ma-0" align="center" justify="center">
+                            <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
+                        </v-row>
+                    </template>
+                </v-img>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
